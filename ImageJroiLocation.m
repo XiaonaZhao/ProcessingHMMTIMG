6227898d03b41c0b808f662bbfc2c7c821474d08
Nona Zhao <s201346044@mail.dlut.edu.cn>
2019-10-30 20:41:51 +0800

    Update ImageJroiLocation.m
    Adding 'Oval' ROI part

--- ImageJroiLocation.m
+++ ImageJroiLocation.m
@@ -9,4 +9,9 @@ switch sROI.strType
         Polygon = sROI.mnCoordinates;
         col = [min(Polygon(:,2)), max(Polygon(:,2))];
         row = [min(Polygon(:,1)), max(Polygon(:,1))];
+    case 'Oval'
+        RectBounds = sROI.vnRectBounds;
+        % [x-left_up y-left_up x-right_down y-right_down]
+        col = [RectBounds(2), RectBounds(4)];
+        row = [RectBounds(1), RectBounds(3)];
 end
