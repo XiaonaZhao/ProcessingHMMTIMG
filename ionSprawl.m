b00d69c5935b47c307a1539a66044a8b8ea7eb37
Nona Zhao <s201346044@mail.dlut.edu.cn>
2019-10-30 20:42:24 +0800

    Update ionSprawl.m
    This file may be deleted later.

--- ionSprawl.m
+++ ionSprawl.m
@@ -83,3 +83,64 @@ end
 %     %     saveas(gcf,[savepath, '\' 'C2_' num2str(ii, '%04d'), '.tif']);
 %     close gcf
 % end
+%%
+[~, tifFile] = uigetfile('*.tiff', 'Multiselect', 'on', 'Read tif Folder');
+tifDir = dir(fullfile(tifFile, '*.tiff'));
+
+tif = cell(101, 1);
+for ii = 138:238
+    tif0 = double(imread(fullfile(tifFile, tifDir(ii).name)));
+    tif1 = double(imread(fullfile(tifFile, tifDir(ii+1).name)));
+    tif{ii-137, 1} = (tif1 - tif0)/2;
+end
+%%
+mask1 = ~imread('G:\TaS2\TaS2_20190520_ITO_AFM\Mask\MaskD5_1.tif');
+mask2 = ~imread('G:\TaS2\TaS2_20190520_ITO_AFM\Mask\MaskD5_2.tif');
+mask3 = ~imread('G:\TaS2\TaS2_20190520_ITO_AFM\Mask\MaskD5_3.tif');
+mask = mask1|mask2|mask3;
+
+figure('color','white');
+imshow(mask, 'DisplayRange',[], 'InitialMagnification', 'fit');
+%%
+X = (1:101)';
+line = zeros(101, 1);
+for ii = 1:101
+    line(ii, 1) = -ROImean(tif{ii, 1}, ~mask);
+end
+figure('color','white');
+plot(X, line);
+xlim([0 100])
+
+%%
+[f1, P1] = fft_P1(line, 10);
+figure('color', 'white');
+plot(f1, P1);
+xlim([0 5])
+title(['Spectrum of ROI ' num2str(ii)])
+xlabel('f (Hz)','fontsize',10)
+ylabel('|P(f)|','fontsize',10)
+
+%%
+area_ion = zeros(101, 1);
+for ii = 1:101
+% ii = 48;
+I = -tif{ii, 1};
+I = I.*mask;
+I = imboxfilt(I, 11);
+h = fspecial('gaussian', 3, 1.5); 
+I = imfilter(I, h);
+I(I < 0) = 0;
+I(I > 70) = 70;
+% I = binary_iterate(I);
+I = binary_bernsen(I);
+% I = binary_otus(I);
+% figure('color','white');
+imshow(I, 'DisplayRange',[], 'InitialMagnification', 'fit');
+title(['Frame ', num2str(ii+138), ' - Frame ', num2str(ii+137)]);
+% colormap jet
+% colorbar;
+% impixelinfo
+% set(gca, 'CLim', [0 70]);
+saveas(gcf,[savepath, 'D5_' num2str(ii, '%04d'), '.tif']);
+% area_ion(ii, 1) = sum(I(:))*((1.25e-9)^2)/(1/100);
+end
