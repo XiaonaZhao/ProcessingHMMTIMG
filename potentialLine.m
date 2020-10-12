119b096a8bfc34e717efc3b0b05311fb5e8777e5
Nona Zhao <nonazhao@mail.ustc.edu.cn>
2020-10-12 20:44:01 +0800

    Update potentialLine.m

--- potentialLine.m
+++ potentialLine.m
@@ -7,5 +7,7 @@ potential2 = ((lowPotential*2 - end1 - scanRate) : (-scanRate) : highPotential)'
 % potential1 = (-0.6 : r : -1.1)';
 % end1 = potential1(end);
 % potential2 = ((-2.2 - end1 - r) : (-r) : -0.6)';
+
 potential = [potential1' potential2' potential1' potential2']'; % 2c
-% Value.potential = [potential1' potential2']';% 1c
+% potential = [potential1' potential2']';% 1c
+end
